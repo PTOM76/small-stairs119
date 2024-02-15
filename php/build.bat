20b9d1a04cd25cc766be5f58c8c1be7de78f68f5
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-15 21:58:43 +0900

    bat

--- php/build.bat
+++ php/build.bat
@@ -6,11 +6,13 @@ git fetch origin
 timeout /T 1
 call gradlew.bat build
 call gradlew.bat generatePomFileForMavenCommonPublication
-call gradlew.bat generatePomFileForMavenForgePublication
 call gradlew.bat generatePomFileForMavenFabricPublication
+call gradlew.bat generatePomFileForMavenForgePublication
+call gradlew.bat generatePomFileForMavenNeoForgePublication
 copy /y ".\common\build\publications\mavenCommon\pom-default.xml" ".\common\build\publications\mavenCommon\%modid%.pom"
 copy /y ".\fabric\build\publications\mavenFabric\pom-default.xml" ".\fabric\build\publications\mavenFabric\%modid%.pom"
 copy /y ".\forge\build\publications\mavenForge\pom-default.xml" ".\forge\build\publications\mavenForge\%modid%.pom"
+copy /y ".\neoforge\build\publications\mavenNeoForge\pom-default.xml" ".\neoforge\build\publications\mavenNeoForge\%modid%.pom"
 
 cd ".\php"
 
